d55391a01e99bbf39edf336cc049570f6c7cf55e
joao neto <joaonetotfm@gmail.com>
2024-08-21 14:52:05 -0300

    feature: home

--- features/home/src/lib/home/data-access/home.service.ts
+++ features/home/src/lib/home/data-access/home.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { UserLoansResponse } from './interfaces/home.interface';
+import { UserLoansResponse } from '../interfaces/home.interface';
 
 @Injectable({ providedIn: 'root' })
 export class HomeService {
